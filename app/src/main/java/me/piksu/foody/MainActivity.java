a551d43ab0d64d97956794926684cc87279f0d46
Daniel <daniel.melander@hotmail.com>
2015-08-29 20:46:24 +0300

    Read me added.

--- app/src/main/java/me/piksu/foody/MainActivity.java
+++ app/src/main/java/me/piksu/foody/MainActivity.java
@@ -2,6 +2,7 @@ package me.piksu.foody;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
